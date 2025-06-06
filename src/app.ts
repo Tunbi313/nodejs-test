f1f9caca3dc9c7649e32e006e065d5c8371e4e7d
tunbi313 <tunbi31032004@gmail.com>
2025-06-06 20:00:00 +0700

    test 9

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
-//onst express = require('express')
+
 import express from "express";
 import 'dotenv/config'
+import { dirname } from "path";
 const app = express()
 const port = process.env.port || 8080;
+ //config view engine
+ app.set('view engine','ejs');
+ app.set('views',__dirname +'/views');
+ console.log(__dirname +'/views')
 
 app.get('/', (req, res) => {
-  res.send('Hello World! tunbi')
+  res.render("home.ejs")
+})
+
+app.get('/abc', (req, res) => {
+  res.send('Hello World! ')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  console.log("env port:",process.env.port)
+  console.log(`Example app listening on port ${port}`);
+  console.log(__dirname +'/views')
 })
